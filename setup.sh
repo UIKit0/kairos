5378185605c9ac86b2504f60c8927785b9949d7c
Vincent Richomme <Vincent@Mac-mini-de-Vincent.local>
2012-01-04 21:12:09 +0100

    simplify setup

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Install cappuccino and cardano
+if [ ! -d ./src/main/webapp/Frameworks ]; then
+    capp gen -f ./src/main/webapp
+fi
+
+if [ ! -d ./misc/cardano/cardano ]; then
+    tar xvf ./misc/cardano/cardano.tar.gz -C ./misc/cardano/
+fi
+
+cp -R ./misc/cardano/cardano/* ./src/main/webapp/Frameworks/
+cp -R ./misc/cardano/cardano/Debug/* ./src/main/webapp/Frameworks/Debug/
