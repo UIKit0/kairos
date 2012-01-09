3792a59c5db8c40de9e4851a36a3241fcbac5c87
Vincent Richomme <Vincent@Mac-mini-de-Vincent.local>
2012-01-09 22:56:13 +0100

    fix setup script

--- setup.sh
+++ setup.sh
@@ -9,5 +9,5 @@ if [ ! -d ./misc/cardano/cardano ]; then
     tar xvf ./misc/cardano/cardano.tar.gz -C ./misc/cardano/
 fi
 
-cp -R ./misc/cardano/cardano/* ./src/main/webapp/Frameworks/
-cp -R ./misc/cardano/cardano/Debug/* ./src/main/webapp/Frameworks/Debug/
+cp -R ./misc/cardano/cardano/Cardano ./src/main/webapp/Frameworks/
+cp -R ./misc/cardano/cardano/Debug ./src/main/webapp/Frameworks/
