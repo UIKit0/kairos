6136dd638d4b2f5c384a96dc824a9696bb1395f4
Victor Kazarinov <oobe@kazarinov.biz>
2012-01-21 03:44:24 +0700

    Added eclipse into gradle. Added RunWebApp.java to start embedded jetty to start it from Eclipse.

--- src/main/java/com/smartmobili/other/MyMain.java
+++
@@ -1,7 +0,0 @@
-package com.smartmobili.other;
-
-public class MyMain {
-  public static void main(String[] args) {
-    System.out.println("Здравствуй, мир!");
-  }
-}
---
+++ src/test/java/RunWebApp.java
@@ -0,0 +1,43 @@
+import org.mortbay.jetty.Connector;
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.servlet.Context;
+import org.mortbay.jetty.webapp.WebAppContext;
+import org.mortbay.jetty.nio.*;
+
+public class RunWebApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Server server = new Server(8080);
+		SelectChannelConnector scc = new SelectChannelConnector();
+
+		scc.setPort(8080);
+		server.setConnectors(new Connector[] { scc });
+
+		WebAppContext context = new WebAppContext();
+		context.setServer(server);
+		context.setContextPath("/");
+		context.setWar("src/main/webapp"); 
+		// here. Need google on jetty and setWar function.
+		server.addHandler(context);
+
+		try {
+			System.out
+					.println(">>> STARTING EMBEDDED JETTY SERVER, PRESS ANY KEY TO STOP");
+			server.start();
+			while (System.in.available() == 0) {
+				Thread.sleep(5000);
+			}
+			server.stop();
+			server.join();
+		} catch (Exception exc) {
+
+			exc.printStackTrace();
+			System.exit(100);
+
+		}
+	}
+
+}
