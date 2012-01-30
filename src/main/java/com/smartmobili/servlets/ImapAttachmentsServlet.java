5fdb17d6c9b64b429a3f52e32a5aea0a7b5a193a
Victor Kazarinov <oobe@kazarinov.biz>
2012-01-31 05:19:26 +0700

    Limited simultaneous thumbnails creation to just one at once, to not overload server memory with many simultaneous thumbnails image creations.

--- src/main/java/com/smartmobili/servlets/ImapAttachmentsServlet.java
+++ src/main/java/com/smartmobili/servlets/ImapAttachmentsServlet.java
@@ -188,6 +188,7 @@ public class ImapAttachmentsServlet extends HttpServlet {
 		}
 	}
 	
+	static String justAnStaticObjectForLock_createThumbnail_Function = new String();
 	/*
 	 * 
 	 * This function is based on code to create thumbnail from code snipped from here: 
@@ -202,74 +203,81 @@ public class ImapAttachmentsServlet extends HttpServlet {
 	 * memory. Perhaps need to create pool and separate conversion thread. There will be a processing queue.
 	 * It will process images for DB and for requests (or everything will be trough DB). So, for example,
 	 * it will be configured to not convert more than 5 images simultaneously. 
+	 * Simplest way to implement is "mutexes" or semaphors with increased counter of simeltanous processing.
+	 * And best if we limit automatically by processors (cores) count, so each core is processing image.
+	 * Bellow for simplification we limit to process only using ONE core with "synchronized" keyword.
 	 * 
 	 * Returns byte array of result thumbnail image or null if some error occured.
 	 */
 	private byte[] createThumbnail(byte[] sourceImage, int thumbWidth,
 			int thumbHeight, int quality) throws InterruptedException,
 			IOException {
-		// load image from filename
-		Image image = Toolkit.getDefaultToolkit().createImage(sourceImage);
-		// Image image = Toolkit.getDefaultToolkit().getImage(filename);
-		MediaTracker mediaTracker = new MediaTracker(new Container());
-		mediaTracker.addImage(image, 0);
-		mediaTracker.waitForID(0);
+		// Why "synchronized" is used please read above in TODO comment in
+		// function header.
+		synchronized (justAnStaticObjectForLock_createThumbnail_Function) {
+			// load image from filename
+			Image image = Toolkit.getDefaultToolkit().createImage(sourceImage);
+			// Image image = Toolkit.getDefaultToolkit().getImage(filename);
+			MediaTracker mediaTracker = new MediaTracker(new Container());
+			mediaTracker.addImage(image, 0);
+			mediaTracker.waitForID(0);
 
-		// use this to test for errors at this point:
-		// System.out.println(mediaTracker.isErrorAny());
-		if (mediaTracker.isErrorAny())
-			return null;
-
-		// determine thumbnail size from WIDTH and HEIGHT
-		double thumbRatio = (double) thumbWidth / (double) thumbHeight;
-		int imageWidth = image.getWidth(null);
-		int imageHeight = image.getHeight(null);
-		double imageRatio = (double) imageWidth / (double) imageHeight;
-		if (thumbRatio < imageRatio) {
-			thumbHeight = (int) (thumbWidth / imageRatio);
-		} else {
-			thumbWidth = (int) (thumbHeight * imageRatio);
-		}
-
-		// draw original image to thumbnail image object and
-		// scale it to the new size on-the-fly
-		Graphics2D graphics2D = null;
-		BufferedImage thumbImage = null;
-		try {
-			thumbImage = new BufferedImage(thumbWidth, thumbHeight,
-				BufferedImage.TYPE_INT_RGB);
-			graphics2D = thumbImage.createGraphics();
-			graphics2D.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
-				RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-			graphics2D.drawImage(image, 0, 0, thumbWidth, thumbHeight, null);
-		}
-		catch (Exception ex) {
-			return null;
-		}
-		finally {
-			graphics2D.dispose();
-		}
-
-		// save thumbnail image
-		ByteArrayOutputStream b = null;
-		try {
-			b = new ByteArrayOutputStream();
-
-			/*JPEGImageEncoder encoder = JPEGCodec.createJPEGEncoder(b);
-			JPEGEncodeParam param = encoder
-					.getDefaultJPEGEncodeParam(thumbImage);
-			quality = Math.max(0, Math.min(quality, 100));
-			param.setQuality((float) quality / 100.0f, false);
-			encoder.setJPEGEncodeParam(param);
-			encoder.encode(thumbImage);*/
-			if (javax.imageio.ImageIO.write(thumbImage, "JPG", b) == false)
+			// use this to test for errors at this point:
+			// System.out.println(mediaTracker.isErrorAny());
+			if (mediaTracker.isErrorAny())
 				return null;
 
-			return b.toByteArray();
-		} catch (Exception ex) {
-			return null;
-		} finally {
-			b.close();
+			// determine thumbnail size from WIDTH and HEIGHT
+			double thumbRatio = (double) thumbWidth / (double) thumbHeight;
+			int imageWidth = image.getWidth(null);
+			int imageHeight = image.getHeight(null);
+			double imageRatio = (double) imageWidth / (double) imageHeight;
+			if (thumbRatio < imageRatio) {
+				thumbHeight = (int) (thumbWidth / imageRatio);
+			} else {
+				thumbWidth = (int) (thumbHeight * imageRatio);
+			}
+
+			// draw original image to thumbnail image object and
+			// scale it to the new size on-the-fly
+			Graphics2D graphics2D = null;
+			BufferedImage thumbImage = null;
+			try {
+				thumbImage = new BufferedImage(thumbWidth, thumbHeight,
+						BufferedImage.TYPE_INT_RGB);
+				graphics2D = thumbImage.createGraphics();
+				graphics2D.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+						RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+				graphics2D
+						.drawImage(image, 0, 0, thumbWidth, thumbHeight, null);
+			} catch (Exception ex) {
+				return null;
+			} finally {
+				graphics2D.dispose();
+			}
+
+			// save thumbnail image
+			ByteArrayOutputStream b = null;
+			try {
+				b = new ByteArrayOutputStream();
+
+				/*
+				 * JPEGImageEncoder encoder = JPEGCodec.createJPEGEncoder(b);
+				 * JPEGEncodeParam param = encoder
+				 * .getDefaultJPEGEncodeParam(thumbImage); quality = Math.max(0,
+				 * Math.min(quality, 100)); param.setQuality((float) quality /
+				 * 100.0f, false); encoder.setJPEGEncodeParam(param);
+				 * encoder.encode(thumbImage);
+				 */
+				if (javax.imageio.ImageIO.write(thumbImage, "JPG", b) == false)
+					return null;
+
+				return b.toByteArray();
+			} catch (Exception ex) {
+				return null;
+			} finally {
+				b.close();
+			}
 		}
 	}
 }
