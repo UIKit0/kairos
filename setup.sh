b7fc74bd9dc124aab133147e468f0a5d52385d2e
Vincent Richomme <Vincent@Mac-mini-de-Vincent.local>
2012-02-05 13:32:46 +0100

    Add cappuccino 0.9.5 framework inside our repository - easier when working from windows because cappuccino doesn't work very well on this platform

--- setup.sh
+++ setup.sh
@@ -4,10 +4,3 @@
 if [ ! -d ./src/main/webapp/Frameworks ]; then
     capp gen -f ./src/main/webapp
 fi
-
-if [ ! -d ./misc/cardano/cardano ]; then
-    tar xvf ./misc/cardano/cardano.tar.gz -C ./misc/cardano/
-fi
-
-cp -R ./misc/cardano/cardano/Cardano ./src/main/webapp/Frameworks/
-cp -R ./misc/cardano/cardano/Debug ./src/main/webapp/Frameworks/
