2df2fbf987d44240cb95746dc5c6359930ca4f92
Vincent Richomme <Vincent@Mac-mini-de-Vincent.local>
2012-08-20 22:49:39 +0200

    update cappuccino framework to 0.9.6-RC1

---
+++ update_frameworks.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+rm -r ./src/main/webapp/Frameworks/AppKit
+rm -r ./src/main/webapp/Frameworks/Foundation
+rm -r ./src/main/webapp/Frameworks/Objective-J
+rm -r ./src/main/webapp/Frameworks/Debug
+
